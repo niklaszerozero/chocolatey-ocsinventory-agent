8d64d53716feadb866b7e6ca3386904c7ac6b664
Niklas Vlach <contact@niklas-vlach.com>
2025-07-07 22:53:09 +0200

    added build.ps1

---
+++ build.ps1
@@ -0,0 +1,5 @@
+choco pack .\ocsinventory-agent\ocsinventory-agent.nuspec
+
+choco pack .\ocsinventory-agent.install\ocsinventory-agent.install.nuspec
+
+choco pack .\ocsinventory-agent.portable\ocsinventory-agent.portable.nuspec
